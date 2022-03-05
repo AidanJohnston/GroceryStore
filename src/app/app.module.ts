f0864145a904bed7c8732e6e2d950d49dc18665b
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-05 16:27:33 -0500

    add button and input module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 
 
@@ -16,11 +17,11 @@ import { LoginComponent } from './pages/login/login.component';
 import { SearchComponent } from './pages/search/search.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
   { path: 'about', component: AboutComponent},
   { path: 'login', component: LoginComponent},
   { path: 'signup', component: SignupComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ]
 
 @NgModule({
@@ -36,8 +37,9 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
+    MatButtonModule,
     MatIconModule,
+    MatInputModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
