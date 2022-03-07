285d2c81dc40a9b1385a39c98e75828938afc06c
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-07 17:22:12 -0500

    boilerplate login/logout/signup functions

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,30 +1,75 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  userState: any;
 
-  async createUserWithPassword() { 
-
+  constructor(
+    public firebaseAuth : AngularFireAuth,
+    public router : Router) { 
+    this.firebaseAuth.authState.subscribe(user => {
+      if (user) {
+        this.userState = user;
+        localStorage.setItem('user', JSON.stringify(this.userState));
+      } else {
+        localStorage.setItem('user', JSON.stringify(null));
+      }
+    })
   }
 
-  async createUserWithGoogle(){
-    
+  createUserWithPassword(email: string, password: string) { 
+    this.firebaseAuth.createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        this.sendVerificationEmail();
+      })
+      .catch(err => {
+        console.warn(err.message);
+      });
   }
 
-  async loginUserWithPassword(){
-
+  sendVerificationEmail() {
+    return this.firebaseAuth.currentUser
+      .then(user => {
+        if(user === null){
+          this.router.navigate(['']);
+        }
+        else{
+          user.sendEmailVerification()
+        }
+      })
+      .then(() => {
+        this.router.navigate(['verify-email']);
+      })
   }
 
-  async loginUserWithGoogle(){
-
+  loginUserWithPassword(email: string, password: string){
+    this.firebaseAuth.signInWithEmailAndPassword(email, password)
+      .then(res =>{
+        this.router.navigate(['']);
+      })
+      .catch(err => {
+        console.warn(err.message);
+      });
   }
 
-  async logoutUser(){
-
+  logoutUser(){
+    this.firebaseAuth.signOut();
   }
 
+  get isUserLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    return (user !== '{}');
+  }
+
+  get isUserEmailVerified(): boolean {
+    const user = JSON.parse(localStorage.getItem('user') || '{}');
+    return (user.emailVerified);
+  }
 }
