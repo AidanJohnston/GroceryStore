4f83956bafb7fc028fd3be2e038a10b98c77ff0f
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-07 20:50:14 -0500

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
-// Enviroment
+// ENVIROMENT
 import { environment } from 'src/environments/environment';
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// ANGULAR MATERIAL
 import { MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import {MatProgressBarModule} from '@angular/material/progress-bar'; 
 import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 // FIREBASE
 import { AngularFireModule } from "@angular/fire/compat";
@@ -16,11 +21,12 @@ import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
+// SERVICES
 import { AuthService } from './services/auth.service';
 import { CartService } from './services/cart.service';
 
+// COMPONENTS
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
@@ -29,17 +35,20 @@ import { LoginComponent } from './pages/login/login.component';
 import { SearchComponent } from './pages/search/search.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './pages/verify-email/verify-email.component';
-import { AuthGuard } from './guards/auth.guard';
+
+// GUARDS
 import { VerifyEmailGuard } from './guards/verify-email.guard';
+import { AuthLoggedinGuard } from './guards/auth-loggedin.guard';
+import { AuthNotloggedinGuard } from './guards/auth-notloggedin.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent , canActivate: [VerifyEmailGuard]},
   { path: 'search', component: SearchComponent, canActivate: [VerifyEmailGuard] },
   { path: 'about', component: AboutComponent, canActivate: [VerifyEmailGuard]},
-  { path: 'login', component: LoginComponent, canActivate: [VerifyEmailGuard]},
-  { path: 'signup', component: SignupComponent, canActivate: [VerifyEmailGuard]},
-  { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [VerifyEmailGuard]},
-  { path: 'verify-email', component: VerifyEmailComponent, canActivate: [AuthGuard, VerifyEmailGuard]}
+  { path: 'login', component: LoginComponent, canActivate: [AuthLoggedinGuard]},
+  { path: 'signup', component: SignupComponent, canActivate: [AuthLoggedinGuard]},
+  { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [AuthNotloggedinGuard]},
+  { path: 'verify-email', component: VerifyEmailComponent, canActivate: [VerifyEmailGuard]}
 ]
 
 @NgModule({
@@ -61,6 +70,8 @@ const routes: Routes = [
     MatButtonModule,
     MatIconModule,
     MatInputModule,
+    MatProgressBarModule,
+    MatSnackBarModule,
     FormsModule,
     RouterModule.forRoot(routes)
   ],
