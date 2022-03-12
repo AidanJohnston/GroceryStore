d15cce8b6fd0988023c86acb53c01eaf36cda634
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-12 16:49:14 -0500

    loading bar + email send

--- src/app/pages/forgot-password/forgot-password.component.ts
+++ src/app/pages/forgot-password/forgot-password.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
-export class ForgotPasswordComponent implements OnInit {
+export class ForgotPasswordComponent {
 
-  constructor() { }
+  constructor(private auth : AuthService) { }
 
-  ngOnInit(): void {
+  loading : boolean = false;
+  error : boolean = false;
+  code : boolean = false;
+
+  emailChangeRequest(submit : NgForm){
+
+    this.loading = true;
+    this.auth.firebaseAuth.sendPasswordResetEmail(submit.value['email'])
+      .then(() => {
+        this.loading = false;
+        this.code = true;
+      }).catch(() => {
+        this.loading = false;
+        this.error = true;
+      })
   }
+
 
 }
