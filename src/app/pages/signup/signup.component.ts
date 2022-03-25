7c87111d0b20b30f9e5298a708c6f1074ad635c6
Aidan Johnston <ajjohnst@lakeheadu.ca>
2022-03-24 20:13:13 -0400

    user model refactor

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-import { Address, User } from 'src/app/services/user.model';
+import { Address, User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-signup',
