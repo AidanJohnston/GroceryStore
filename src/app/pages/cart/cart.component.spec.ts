069aab3d5e03929100424c9331330337f442925d
mishalmartin <78604712+mishalmartin@users.noreply.github.com>
2022-03-28 16:17:22 -0400

    cart page

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ItemComponent } from './item.component';
+import { CartComponent } from './cart.component';
 
-describe('ItemComponent', () => {
-  let component: ItemComponent;
-  let fixture: ComponentFixture<ItemComponent>;
+describe('CartComponent', () => {
+  let component: CartComponent;
+  let fixture: ComponentFixture<CartComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ItemComponent ]
+      declarations: [ CartComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ItemComponent);
+    fixture = TestBed.createComponent(CartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart/cart.service';
+import { Cart } from '../../models/Cart';
+import { CartItem } from '../../models/CartItem';
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartPageComponent implements OnInit {
+  cart!:Cart;
+  constructor(private cartService: CartService) { 
+    this.setCart();
+  }
+  ngOnInit(): void {
+  }
+
+  removeFromCart(cartItem:CartItem){
+    this.cartService.removeFromCart(cartItem.food.id);
+    this.setCart();
+  }
+
+  changeQuantity(cartItem:CartItem, quantityInString:string){
+    const quantity= parseInt(quantityInString);
+    this.cartService.changeQuantity(cartItem.food.id, quantity);
+    this.setCart();
+  }
+
+  setCart(){
+    this.cart = this.cartService.getCart();
+  }
+
+}
