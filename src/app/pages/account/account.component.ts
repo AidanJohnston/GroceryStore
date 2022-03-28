99bb38e58a5a3cf8ab7c43251db0e3b991897e23
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-28 00:19:19 -0400

    account init

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Transaction } from 'src/app/models/transaction.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-account',
@@ -7,13 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(private afs : AngularFirestore,
+    private angularFireAuth : AngularFireAuth) { }
 
   hide: boolean = true;
   isError: boolean = false;
 
+  transaction!: Transaction[];
 
   ngOnInit(): void {
+    this.getTransactions().then(res => {
+      this.transaction = res;
+    });
+
+    console.log(this.transaction);
   }
 
   infoSubmit(form: any){
@@ -21,7 +32,21 @@ export class AccountComponent implements OnInit {
   }
 
   accountSubmit(form: any){
-    
+
   }
 
+  async getTransactions() : Promise<Transaction[]> {
+
+    let documents : Transaction[] = [];
+
+    var user = await this.angularFireAuth.currentUser;
+    if(user){
+      var userRef = this.afs.collection('users').doc(user.uid);
+      var transactions = await userRef.collection('transactions').ref.get();
+      transactions.forEach(doc => {
+        documents.push(doc.data() as Transaction);
+      });
+    }
+    return documents;
+  }
 }
