228ff813b460c19bf9efb6fe8107b8b899f55706
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-28 19:59:42 -0400

    cart display

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -2,18 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { Cart } from '../../models/cart.model';
 import { CartItem } from '../../models/cartItem.model';
+import { NgForm } from '@angular/forms';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AuthService } from 'src/app/services/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  cart!:Cart;
-  
-  constructor(private cartService: CartService,
-    ) { 
-    this.setCart();
-  }
+
+  constructor(
+    private cartService: CartService,
+    private afs : AngularFirestore,
+    private authService : AuthService,
+    public snackBar : MatSnackBar) { }
+
+  cartItems = this.cartService.getCartItems();
+  total = this.cartService.totalPrice;
+  isLoading : boolean = false;
+  isError : boolean = false;
+
+  displayedColumns = ['name', 'price', 'quantity', 'subtotal', 'remove'];
+
   ngOnInit(): void {
   }
 
@@ -28,12 +41,39 @@ export class CartComponent implements OnInit {
     this.setCart();
   }
 
-  setCart(){
-    this.cart = this.cartService.getCart();
-  }
-
-  checkout(){
+  checkout(form : NgForm){
     
+    this.isLoading = true;
+
+    const data = {
+      address: {
+        address_1: form.value.address,
+        city: form.value.city,
+        province: form.value.province,
+        postalCode: form.value.postalCode
+      }, 
+      time: new Date().getTime(),
+      cart: this.cartItems,
+      total: this.total
+    }
+    
+    this.isLoading = true;
+    this.afs.collection('users').doc(this.authService.getUser().uid).collection('transactions').add(data).then(res => {
+        this.isError = false;
+        this.isLoading = false;
+  
+        this.snackBar.open('Transaction Submitted', 'Close', {
+          duration: 2000,
+        });
+  
+      }).catch(err => {
+        this.isError = true;
+        this.isLoading = false;
+      });
+  }
+  setCart(){
+    this.cartItems = this.cartService.getCartItems();
+    this.total = this.cartService.totalPrice;
   }
 
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -2,35 +2,44 @@ import { Injectable } from '@angular/core';
 import { Cart } from 'src/app/models/cart.model';
 import { CartItem } from 'src/app/models/cartItem.model';
 import { Item } from 'src/app/models/item.model';
+import { Transaction } from '../models/transaction.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  private cart:Cart = new Cart();
+  items:CartItem[] = [];
+
+    get totalPrice(): number{
+        let totalPrice = 0;
+        this.items.forEach(item => {
+            totalPrice += item.price;
+        });
+
+        return totalPrice;
+    }
   
   addToCart(item: Item):void{
-    let cartItem = this.cart.items.find(value => value.item.id === item.id);
+    let cartItem = this.items.find(value => value.item.id === item.id);
     if(cartItem){
       this.changeQuantity(item.id, cartItem.quantity + 1);
       return;
     }
-    this.cart.items.push(new CartItem(item));
+    this.items.push(new CartItem(item));
   }
 
   removeFromCart(itemID : string): void{
-    this.cart.items = 
-    this.cart.items.filter(value => value.item.id != itemID);
+    // TODO
   }
 
   changeQuantity(itemID: string, quantity:number){
-    let cartItem = this.cart.items.find(value => value.item.id === itemID);
+    let cartItem = this.items.find(value => value.item.id === itemID);
     if(!cartItem) return;
     cartItem.quantity = quantity;
   }
 
-  getCart():Cart{
-    return this.cart;
+  getCartItems():CartItem[]{
+    return this.items;
   }
 }
