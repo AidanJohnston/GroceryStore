c0e7c62db8ba56a7b43014ffb4124df25a93102c
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-28 14:33:18 -0400

    addTransaction()

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Transaction } from '../models/transaction.model';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+  constructor(
+    private afs : AngularFirestore,
+    private authService : AuthService) { }
+
+
+  async addTransaction(transaction: Transaction) : Promise<boolean> {
+
+    var user = this.authService.getUser();
+
+    return this.afs.collection('users').doc(user.uid).collection('transactions').add(transaction).then(res => {
+        if(res)
+          return true;
+        else
+          return false;
+      }).catch(err => {
+        return false;
+      });
+  }
 }
