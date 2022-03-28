8cffb08b5d7bcced18049023f091f7a313d3ca19
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-28 14:32:53 -0400

    account page functianlty

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Transaction } from 'src/app/models/transaction.model';
+import { Address, User } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -12,34 +15,66 @@ import { AuthService } from 'src/app/services/auth.service';
 export class AccountComponent implements OnInit {
 
   constructor(private afs : AngularFirestore,
-    private angularFireAuth : AngularFireAuth) { }
+    private authService : AuthService,
+    public snackBar : MatSnackBar) { }
 
   hide: boolean = true;
   isError: boolean = false;
+  isLoading: boolean = false;
 
-  transaction!: Transaction[];
+  transaction: Transaction[] = [];
+  user : User = {
+    address: {} as Address,
+  } as User;
 
   ngOnInit(): void {
-    this.getTransactions().then(res => {
-      this.transaction = res;
+
+    this.getUserInfo().then(res => {
+      this.user = res as User;
     });
 
-    console.log(this.transaction);
+    this.getTransactions().then(res => {
+      this.transaction = res as Transaction[];
+    });
   }
 
-  infoSubmit(form: any){
+  infoSubmit(form: NgForm){
 
+    const data = {
+      fName: form.value['fName'],
+      lName: form.value['lName'],
+      address: {
+        address_1: form.value['address'],
+        city: form.value['city'],
+        postalCode: form.value['postalCode'],
+        province: form.value['province']
+      }
+    }
+
+    console.log(data);
+
+    this.isLoading = true;
+    this.afs.collection('users').doc(this.authService.getUser().uid).update(data).then(res => {
+      this.isError = false;
+      this.isLoading = false;
+
+      this.snackBar.open('Account Information Updated', 'Close', {
+        duration: 2000,
+      });
+
+    }).catch(err => {
+      this.isError = true;
+      this.isLoading = false;
+    });
   }
 
-  accountSubmit(form: any){
-
-  }
 
   async getTransactions() : Promise<Transaction[]> {
 
     let documents : Transaction[] = [];
 
-    var user = await this.angularFireAuth.currentUser;
+    var user = this.authService.getUser();
+
     if(user){
       var userRef = this.afs.collection('users').doc(user.uid);
       var transactions = await userRef.collection('transactions').ref.get();
@@ -48,5 +83,17 @@ export class AccountComponent implements OnInit {
       });
     }
     return documents;
+  }
+
+  async getUserInfo() : Promise<User | undefined> {
+
+    var user = this.authService.getUser();
+
+    if(user){
+      var userRef = this.afs.collection('users').doc(user.uid);
+      var userInfo = await userRef.ref.get();
+      return userInfo.data() as User;
+    }
+    return undefined;
   }
 }
