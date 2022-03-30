992640c795cfab9faf3e6f458a2a2d7369f9a1b1
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-30 11:09:09 -0400

    verify email tests

--- src/app/pages/verify-email/verify-email.component.spec.ts
+++ src/app/pages/verify-email/verify-email.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from 'src/environments/environment';
 
 import { VerifyEmailComponent } from './verify-email.component';
 
@@ -8,7 +13,11 @@ describe('VerifyEmailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ VerifyEmailComponent ]
+      declarations: [ VerifyEmailComponent ],
+      imports: [AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        RouterTestingModule,
+        MatSnackBarModule]
     })
     .compileComponents();
   });
