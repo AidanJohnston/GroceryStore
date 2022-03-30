73586008ce842f8d5c3b457a21275e8d7d4eb0e8
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-30 11:07:04 -0400

    auth logged in tests

--- src/app/guards/auth-loggedin.guard.spec.ts
+++ src/app/guards/auth-loggedin.guard.spec.ts
@@ -1,4 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../services/auth.service';
 
 import { AuthLoggedinGuard } from './auth-loggedin.guard';
 
@@ -6,7 +11,12 @@ describe('AuthLoggedinGuard', () => {
   let guard: AuthLoggedinGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AuthService],
+      imports: [AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        RouterTestingModule]
+    });
     guard = TestBed.inject(AuthLoggedinGuard);
   });
 
