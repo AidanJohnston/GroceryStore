b1db1f5570fecb1cffbb143dcaf70451c2514cbb
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-30 11:07:27 -0400

    verify email gaurd tests

--- src/app/guards/verify-email.guard.spec.ts
+++ src/app/guards/verify-email.guard.spec.ts
@@ -1,4 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from 'src/environments/environment';
+import { AuthService } from '../services/auth.service';
 
 import { VerifyEmailGuard } from './verify-email.guard';
 
@@ -6,7 +11,12 @@ describe('VerifyEmailGuard', () => {
   let guard: VerifyEmailGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AuthService],
+      imports: [AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        RouterTestingModule]
+    });
     guard = TestBed.inject(VerifyEmailGuard);
   });
 
