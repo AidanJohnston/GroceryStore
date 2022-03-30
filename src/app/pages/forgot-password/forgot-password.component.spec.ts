282b74efc1b322ceefddd32a75f653970d7e7478
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-30 11:07:56 -0400

    forgot password tests

--- src/app/pages/forgot-password/forgot-password.component.spec.ts
+++ src/app/pages/forgot-password/forgot-password.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from 'src/environments/environment';
 
 import { ForgotPasswordComponent } from './forgot-password.component';
 
@@ -8,7 +13,12 @@ describe('ForgotPasswordComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ForgotPasswordComponent ]
+      declarations: [ ForgotPasswordComponent ],
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        RouterTestingModule,
+        FormsModule]
     })
     .compileComponents();
   });
