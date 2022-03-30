e1476e8146054fdcf3981b4d98d8f53923692bc2
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-30 11:07:45 -0400

    cart tests

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -1,4 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from 'src/app/services/auth.service';
+import { environment } from 'src/environments/environment';
 
 import { CartComponent } from './cart.component';
 
@@ -8,7 +15,14 @@ describe('CartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CartComponent ]
+      declarations: [ CartComponent ],
+      providers: [ AuthService ],
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        RouterTestingModule,
+        FormsModule,
+        MatSnackBarModule]
     })
     .compileComponents();
   });
