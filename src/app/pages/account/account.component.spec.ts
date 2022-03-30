59b690edc773fc29d933bd663af20468fc8e90fa
Aidan Johnston <aidanjhnston@gmail.com>
2022-03-30 11:07:38 -0400

    account tests

--- src/app/pages/account/account.component.spec.ts
+++ src/app/pages/account/account.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { FormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
+import { environment } from 'src/environments/environment';
 
 import { AccountComponent } from './account.component';
 
@@ -8,7 +14,14 @@ describe('AccountComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AccountComponent ]
+      declarations: [ AccountComponent ],
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        RouterTestingModule,
+        MatSnackBarModule,
+        FormsModule
+      ]
     })
     .compileComponents();
   });
